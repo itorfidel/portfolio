d07e74d4be00418f96cd7060e713d34a7d217d65
Itor Fidelis <jnrfidelisitor@gmail.com>
2025-08-23 22:28:27 +0100

    first commit

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -94,7 +94,7 @@ const Home = () => {
             {/* <p>I'm</p> */}
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga,
-              incidunt hic? Quo quis animi explicabo.jn[dofnoiqoe]
+              incidunt hic? Quo quis animi explicabo.
             </p>
             <ul>
               <li>
